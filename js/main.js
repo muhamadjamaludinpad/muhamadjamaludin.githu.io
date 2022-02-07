36df05ee5f54f2a7f2e4aedbb9b91734bb8fa8cd
Muhamad Jamaludin <muhhjam@gmail.com>
2022-02-07 14:41:45 +0700

    mengubah tampilan pictures

--- js/main.js
+++ js/main.js
@@ -148,3 +148,14 @@
     skillsWayPoint();
   });
 })();
+
+// readmore
+$('.readmore-btn').on('click', function () {
+  $(this).parent().toggleClass('showContent');
+
+  // Shorthand if-else statement
+  var replaceText = $(this).parent().hasClass('showContent')
+    ? '<- Read Less'
+    : 'Read More ->';
+  $(this).text(replaceText);
+});
